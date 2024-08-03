206cf1eb41d1e6241e11fb7c122d3c814efc6537
summer <80736033+yl9517@users.noreply.github.com>
2024-08-03 22:34:35 +0900

    fix: buddypage 명칭 변경

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import MainPage from 'pages/main';
 import LoginPage from 'pages/login';
 import SearchPage from 'pages/search';
 import TestPage from 'pages/test';
-import BuddyPage from 'pages/buddy';
+import BuddyListPage from 'pages/buddy';
 import LinkPage from 'pages/link';
 import LinkDetailPage from 'pages/link/detail';
 
@@ -27,29 +27,28 @@ function App() {
 
   return (
     <div className="App">
-
-      <Flex justify='center'>
-      <LayoutComponent>
-      <ConfigProvider theme={antdTheme}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/my" element={<MyLinkPage />} />
-            <Route path="/buddylist" element={<BuddyPage />} />
-            <Route path="/buddy" element={<BuddyPage />} />
-            <Route path="/user" element={<UserPage />} />
-            <Route path="/user/:userId" element={<UserEditPage />} />   
-            <Route path="/add-link" element={<AddLinkPage />} />
-            <Route path="/main" element={<MainPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/test" element={<TestPage />} />
-            <Route path="/link" element={<LinkPage />} />
-            <Route path="/link/:id" element={<LinkDetailPage />} />
-        </Routes>
-      </ConfigProvider>
-      </LayoutComponent>
+      <Flex justify="center">
+        <LayoutComponent>
+          <ConfigProvider theme={antdTheme}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/my" element={<MyLinkPage />} />
+              <Route path="/buddylist" element={<BuddyListPage />} />
+              <Route path="/buddy" element={<BuddyPage />} />
+              <Route path="/user" element={<UserPage />} />
+              <Route path="/user/:userId" element={<UserEditPage />} />
+              <Route path="/add-link" element={<AddLinkPage />} />
+              <Route path="/main" element={<MainPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/test" element={<TestPage />} />
+              <Route path="/link" element={<LinkPage />} />
+              <Route path="/link/:id" element={<LinkDetailPage />} />
+            </Routes>
+          </ConfigProvider>
+        </LayoutComponent>
       </Flex>
     </div>
   );
