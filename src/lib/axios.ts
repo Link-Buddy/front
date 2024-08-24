b7f17cc05053d6ac567157b3088083165ce04b7a
yjink12 <yjin.k12@gmail.com>
2024-08-24 22:46:32 +0900

    feat: 버디 회원 리스트 조회 & 친구 초대 - [x] 기능 추가 - [ ] 기능 수정 - [ ] 기능 삭제 - [ ] 버그 수정 - [ ] 리팩토링, 코드 포맷팅 - [ ] CI/CD, 환경변수 - [ ] 문서 작업 resolves

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = 'http://localhost:8080';
-const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMTIyNTI5MzA5OTg1NDczNDQ5MzMiLCJhdXRoIjoiVVNFUiIsImV4cCI6MTcyMzkwMDM3Nn0.cU4AbAXEVtgIByv5y1sEzkmyEXME9n1Q_JOfdb8ekXQ';
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ5dXllbzA0NDhAZ21haWwuY29tIiwiYXV0aCI6IiIsImV4cCI6MTcyNDU2NzA3N30.FwQv_t7BhHCq5WFI7yQJd38X-3j38hQJcyKs4ReKR5M';
 
 // axios instance 생성
 export const axiosInstance = axios.create({
