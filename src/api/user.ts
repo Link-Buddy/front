05a1cc4abe2ebe76f7bfc678c416e3960f60373d
yjink12 <yjin.k12@gmail.com>
2024-09-07 22:26:53 +0900

    feat : 친구 초대 - 회원검색 API 연결 & 버디 리스트 조회 API 수정 (userId 삭제) - [x] 기능 추가 - [x] 기능 수정 - [ ] 기능 삭제 - [ ] 버그 수정 - [ ] 리팩토링, 코드 포맷팅 - [ ] CI/CD, 환경변수 - [ ] 문서 작업 resolves

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,8 +1,8 @@
-import { axiosInstance } from "lib/axios"
+import { axiosInstance } from 'lib/axios';
 
 /** 회원 리스트 조회 (검색) */
-export const getUserList = async (userData: any): Promise<any> => {
-    const params = { userData: userData };
+export const getUserList = async (email: string): Promise<any> => {
+    const params = { email: email };
     const { data } = await axiosInstance.get('/user', { params });
     return data;
-}
+};
