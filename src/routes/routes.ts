8322354216125427831a7fa1afd52cfbf75db98a
summer <80736033+yl9517@users.noreply.github.com>
2024-11-10 21:12:42 +0900

    최근본 페이지 route 추가

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,6 +8,7 @@ import AddLinkForm from 'pages/link/add';
 import LinkDetailPage from 'pages/link/detail';
 import LoginPage from 'pages/login';
 import HomePage from 'pages/main';
+import RecentViewPage from 'pages/recentview';
 import RegistedPage from 'pages/registed';
 import SearchPage from 'pages/search';
 import TestPage from 'pages/test';
@@ -15,72 +16,76 @@ import UserPage from 'pages/user';
 import UserEditPage from 'pages/user/edit';
 
 export const routes = [
-    {
-        path: '/',
-        element: HomePage,
-    },
-    {
-        path: '/',
-        element: HomePage,
-    },
-    {
-        path: '/home',
-        element: HomePage,
-    },
-    {
-        path: '/my',
-        element: MyLinkPage,
-    },
-    {
-        path: '/buddy/:buddyId',
-        element: BuddyPage,
-    },
-    {
-        path: '/buddy/list',
-        element: BuddyListPage,
-    },
-    {
-        path: '/buddy/invitation',
-        element: BuddyInvitationPage,
-    },
-    {
-        path: '/user/:userId',
-        element: UserPage,
-    },
-    {
-        path: '/user/edit/my',
-        element: UserEditPage,
-    },
-    {
-        path: '/add-link',
-        element: AddLinkForm,
-    },
-    {
-        path: '/add-buddy',
-        element: AddBuddyPage,
-    },
-    {
-        path: '/login',
-        element: LoginPage,
-    },
-    {
-        path: '/search',
-        element: SearchPage,
-    },
-    {
-        path: '/favorite',
-        element: FavoritePage,
-    },
-    {
-        path: '/registed',
-        element: RegistedPage,
-    },
-    {
-        path: '/test',
-        element: TestPage,
-    },
-    {
-        path: '/category/:categoryId',
-        element: LinkDetailPage,
-    },
+  {
+    path: '/',
+    element: HomePage,
+  },
+  {
+    path: '/',
+    element: HomePage,
+  },
+  {
+    path: '/home',
+    element: HomePage,
+  },
+  {
+    path: '/my',
+    element: MyLinkPage,
+  },
+  {
+    path: '/buddy/:buddyId',
+    element: BuddyPage,
+  },
+  {
+    path: '/buddy/list',
+    element: BuddyListPage,
+  },
+  {
+    path: '/buddy/invitation',
+    element: BuddyInvitationPage,
+  },
+  {
+    path: '/user/:userId',
+    element: UserPage,
+  },
+  {
+    path: '/user/edit/my',
+    element: UserEditPage,
+  },
+  {
+    path: '/add-link',
+    element: AddLinkForm,
+  },
+  {
+    path: '/add-buddy',
+    element: AddBuddyPage,
+  },
+  {
+    path: '/login',
+    element: LoginPage,
+  },
+  {
+    path: '/search',
+    element: SearchPage,
+  },
+  {
+    path: '/favorite',
+    element: FavoritePage,
+  },
+  {
+    path: '/registed',
+    element: RegistedPage,
+  },
+  {
+    path: '/recent-view',
+    element: RecentViewPage,
+  },
+  {
+    path: '/test',
+    element: TestPage,
+  },
+  {
+    path: '/category/:categoryId',
+    element: LinkDetailPage,
+  },
 ];
