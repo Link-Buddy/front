8b49f59c88880c10985f66cbe1528258ddb3daa7
yjink12 <yjin.k12@gmail.com>
2024-12-15 09:10:51 +0900

    [FIX] 회원가입 validation 추가 & 로그인, 회원가입 화면 수정 & 회원가입 후 로그인 화면 이동 팝업 추가 - [x] 기능 추가 - [x] 기능 수정 - [ ] 기능 삭제 - [ ] 버그 수정 - [ ] 리팩토링, 코드 포맷팅 - [ ] CI/CD, 환경변수 - [ ] 문서 작업 resolves

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -8,15 +8,28 @@ import { useLocation, useNavigate } from 'react-router-dom';
 const LayoutComponent = ({ children }: any) => {
   const navigate = useNavigate();
   const location = useLocation();
+  // 뒤로가기 버튼
   const [showBackBtn, setShowBackBtn] = useState<boolean>(false);
-  const showBackBtnPath = ['/category', '/buddy'];
+  const showBackBtnPath = ['/category', '/buddy', '/user/join'];
   const hideBackBtnPath = ['/buddy/list'];
+  // NavBar
+  const [showNavBarPath, setShowNavBarPath] = useState<boolean>(true);
+  const hideNavBarPath = ['/login', '/user/join'];
 
   const handleBackButton = () => {
     navigate(-1);
   };
 
   useEffect(() => {
+    // NavBar 노출 페이지인지 확인
+    const checkHideNavBarPath = hideNavBarPath.some((path) =>
+      location.pathname.startsWith(path)
+    );
+    if (checkHideNavBarPath) {
+      setShowNavBarPath(false);
+    } else {
+      setShowNavBarPath(true);
+    }
     // 뒤로가기 버튼 노출 페이지인지 확인
     const checkShowBackBtnPath =
       showBackBtnPath.some((path) => location.pathname.startsWith(path)) &&
@@ -31,14 +44,14 @@ const LayoutComponent = ({ children }: any) => {
   }, [location.pathname]);
 
   return (
-    <Layout className="relative max-w-screen-md">
+    <Layout className="relative max-w-screen-md h-dvh">
       <Content
         style={{
           backgroundColor: 'white',
-          padding: '40px 0px 80px 0px',
+          padding: '40px 0px 40px 0px',
           // maxHeight: 'calc(100vh - 144px)',
           // maxHeight: 'auto',
-          height: '100%',
+          // height: '100%',
           overflowY: 'auto',
         }}
       >
@@ -59,7 +72,7 @@ const LayoutComponent = ({ children }: any) => {
         )}
         {children}
       </Content>
-      <NavBar />
+      {showNavBarPath && <NavBar />}
     </Layout>
   );
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Col, Divider, Flex, Form, Input, Row, Typography } from 'antd';
 import { signIn } from 'api/auth';
 import { useState } from 'react';
@@ -51,115 +52,117 @@ const LoginPage = () => {
         height: '100%',
         padding: '0px 25px 50px 25px',
         display: 'flex',
-        // alignItems: 'center',
         flexDirection: 'column',
+        justifyContent: 'space-between',
       }}
     >
-      <Typography.Title
-        level={4}
-        style={{ paddingBottom: 8, textAlign: 'center' }}
-      >
-        로그인
-      </Typography.Title>
-      <Form
-        name="basic"
-        layout="vertical"
-        // labelCol={{
-        //   span: 8,
-        // }}
-        // wrapperCol={{
-        //   span: 16,
-        // }}
-        // style={{
-        //   maxWidth: 600,
-        // }}
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          style={{ width: '100%' }}
-          label="email"
-          name="email"
-          validateStatus={loginError ? 'error' : ''} // 에러 상태일 때 에러 스타일 적용
-          rules={[
-            {
-              required: true,
-              message: 'Please input your email!',
-            },
-          ]}
+      <div>
+        <Row justify={'center'} style={{ paddingBottom: 80, paddingTop: 80 }}>
+          <img src="/images/logo_main.png" style={{ width: 170 }} />
+        </Row>
+      </div>
+      <div style={{ paddingTop: 50 }}>
+        <Form
+          name="basic"
+          layout="vertical"
+          // labelCol={{
+          //   span: 8,
+          // }}
+          // wrapperCol={{
+          //   span: 16,
+          // }}
+          // style={{
+          //   maxWidth: 600,
+          // }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input
-            type="email"
-            placeholder="이메일"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          validateStatus={loginError ? 'error' : ''}
-          help={loginError} // 에러 메시지 표시
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder="비밀번호"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item style={{}}>
-          <Button
-            block
-            size="large"
-            type="primary"
-            htmlType="submit"
-            loading={loading}
-            className="mt-5"
-            onClick={handleSignIn}
+          <Form.Item
+            style={{ width: '100%' }}
+            // label="email"
+            name="email"
+            validateStatus={loginError ? 'error' : ''} // 에러 상태일 때 에러 스타일 적용
+            rules={[
+              {
+                required: true,
+                message: '이메일을 입력해주세요!',
+              },
+            ]}
           >
-            로그인
-          </Button>
-        </Form.Item>
-        <Typography.Text>
-          <a onClick={() => navigate('/user/join')}>회원가입</a>
-        </Typography.Text>
-      </Form>
-      <div style={{ paddingTop: 40 }}>
+            <Input
+              size="large"
+              type="email"
+              placeholder="이메일"
+              onChange={(e) => setEmail(e.target.value)}
+              prefix={<UserOutlined />}
+            />
+          </Form.Item>
+          <Form.Item
+            // label="Password"
+            name="password"
+            validateStatus={loginError ? 'error' : ''}
+            help={loginError} // 에러 메시지 표시
+            rules={[
+              {
+                required: true,
+                message: '비밀번호를 입력해주세요!',
+              },
+            ]}
+          >
+            <Input.Password
+              size="large"
+              placeholder="비밀번호"
+              onChange={(e) => setPassword(e.target.value)}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Row justify={'end'}>
+            <Typography.Text>
+              <a onClick={() => navigate('/user/join')}>회원가입</a>
+            </Typography.Text>
+          </Row>
+          <Form.Item style={{ paddingTop: 20 }}>
+            <Button
+              block
+              size="large"
+              type="primary"
+              htmlType="submit"
+              loading={loading}
+              className="mt-5"
+              onClick={handleSignIn}
+              style={{ fontWeight: 'bold', height: 50, fontSize: 17 }}
+            >
+              로그인
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+      <div style={{ paddingTop: 10 }}>
         {/* 소셜 로그인 */}
         <Divider>
           <Typography.Text strong>간편로그인</Typography.Text>
         </Divider>
         <Flex justify="center">
-          <Row>
-            <Col>
-              <Button
-                type="default"
-                onClick={() => handleSocialLogin('google')}
-                style={{
-                  width: '100%',
-                  maxWidth: '200px',
-                  backgroundColor: '#4285F4',
-                  color: 'white',
-                  fontWeight: 'bold',
-                }}
-              >
-                Google로 로그인
-              </Button>
+          <Row justify={'space-between'}>
+            <Col style={{ paddingLeft: 8 }}>
+              <div>
+                <img
+                  src="/images/google_login_btnSI.png"
+                  onClick={() => handleSocialLogin('google')}
+                  style={{ cursor: 'pointer', height: 50, width: 170 }}
+                />
+              </div>
             </Col>
             <Col style={{ paddingLeft: 8 }}>
               <div>
                 <img
                   src="/images/naver_login_btnG.png"
                   onClick={() => handleSocialLogin('naver')}
-                  style={{ cursor: 'pointer', height: 50 }}
+                  style={{ cursor: 'pointer', height: 50, width: 170 }}
                 />
               </div>
             </Col>
